87fbfcccc34a5e0de41b539b4255f4ca17a90a61
Yuna Takele <115326375+YunaTakele@users.noreply.github.com>
2025-05-01 19:41:02 -0700

    Added prework files to the repository

---
+++ CodePath preworkTests/CodePath_preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePath_preworkTests.swift
+//  CodePath preworkTests
+//
+//  Created by Yuna Takele on 4/30/25.
+//
+
+import Testing
+@testable import CodePath_prework
+
+struct CodePath_preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
